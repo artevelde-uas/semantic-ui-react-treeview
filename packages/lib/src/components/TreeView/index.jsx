8ca574a9c36d778e1ff62a84cb7db96e884f8fa0
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-08-18 08:55:55 +0200

    Render the tree

--- packages/lib/src/components/TreeView/index.jsx
+++ packages/lib/src/components/TreeView/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import { Checkbox } from 'semantic-ui-react';
 
 import styles from './index.module.css';
 
@@ -10,10 +12,42 @@ import styles from './index.module.css';
  * 
  */
 export default ({
+    name,
+    data: initialData = [],
     ...props
 }) => {
+    const [data, setData] = useState(initialData);
+
+    const ItemList = ({ items }) => (
+        <ul className={styles.list}>
+            {items.map((item, index) => (
+                <Item key={index} item={item} />
+            ))}
+        </ul>
+    );
+
+    const Item = ({ item }) => (
+        <li className={styles.item}>
+            <label>
+                <Checkbox
+                    className={styles.checkbox}
+                    checked={item.checked}
+                    name={item.name}
+                />
+                {item.content}
+            </label>
+            {item.children && (
+                <ItemList items={item.children} />
+            )}
+        </li>
+    );
+
     return (
-        <div {...props} className={styles.treeview}>
+        <div
+            {...props}
+            className={styles.treeview}
+        >
+            <ItemList items={data} />
         </div>
     );
 };
