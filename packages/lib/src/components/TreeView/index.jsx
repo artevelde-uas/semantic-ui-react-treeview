0ae956a3ebcbff2a667495d7fe5805ff699836b2
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-08-19 11:18:17 +0200

    Handle item clicks

--- packages/lib/src/components/TreeView/index.jsx
+++ packages/lib/src/components/TreeView/index.jsx
@@ -19,30 +19,67 @@ export default ({
     const [itemRefs, setItemRefs] = useState(new Map());
 
     useEffect(() => {
-        function processItems(items = [], parent = null) {
-            return items.map(({ name, children, ...props }) => {
-                const item = {
-                    ...props,
-                    name,
-                    parent
-                };
-
-                // Recursively process the child items if present
-                if (Array.isArray(children)) {
-                    item.children = processItems(children, item);
-                }
-
-                // Store a reference to the item
-                itemRefs.set(name, item);
-
-                return item;
-            });
-        }
-
         const data = processItems(initialData);
 
         setData(data);
     }, initialData);
+
+    function processItems(items = [], parent = null) {
+        return items.map(({ name, children, ...props }) => {
+            const item = {
+                ...props,
+                name,
+                parent
+            };
+
+            // Recursively process the child items if present
+            if (Array.isArray(children)) {
+                item.children = processItems(children, item);
+            }
+
+            // Store a reference to the item
+            itemRefs.set(name, item);
+
+            return item;
+        });
+    }
+
+    function setParentStates({ parent }) {
+        if (parent === null) return;
+
+        const siblings = parent.children;
+        const checkedCount = siblings.reduce(
+            (count, { checked }) => (count + (checked ? 1 : 0)), 0
+        );
+        const hasIndeterminate = siblings.some(item => item.indeterminate);
+
+        parent.checked = checkedCount === siblings.length;
+        parent.indeterminate = hasIndeterminate || (0 < checkedCount && checkedCount < siblings.length);
+
+        setParentStates(parent);
+    }
+
+    function setChildStates({ children, checked }) {
+        if (!Array.isArray(children)) return;
+
+        children.forEach(item => {
+            item.checked = checked;
+            item.indeterminate = false;
+            setChildStates(item);
+        });
+    }
+
+    function handleClick(event, { name, checked }) {
+        const item = itemRefs.get(name);
+
+        item.checked = checked;
+        item.indeterminate = false;
+
+        setChildStates(item);
+        setParentStates(item);
+
+        setItemRefs(new Map(itemRefs));
+    }
 
     const ItemList = ({ items }) => (
         <ul className={styles.list}>
@@ -58,7 +95,9 @@ export default ({
                 <Checkbox
                     className={styles.checkbox}
                     checked={item.checked}
+                    indeterminate={item.indeterminate}
                     name={item.name}
+                    onClick={handleClick}
                 />
                 {item.content}
             </label>
